4a64b2a8dafe259df3f1e8ac8d1fa50513d15442
Maxim Ivanov <quckly.max@gmail.com>
2019-05-24 22:48:46 +0300

    Added speed measurement.

--- src/main/kotlin/dev/maxiv/fuzzer/http/Main.kt
+++ src/main/kotlin/dev/maxiv/fuzzer/http/Main.kt
@@ -8,6 +8,7 @@ import com.github.ajalt.clikt.parameters.options.required
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 @ExperimentalCoroutinesApi
 fun main(args: Array<String>) {
@@ -25,15 +26,25 @@ fun main(args: Array<String>) {
 class ProgramConfigurator : CliktCommand(name = "http-fuzzer") {
     val targetIP: String by option(help = "IP address of http(s) server", metavar = "IP").required()
     val baseDomain: String by option(help = "Base domain of site for bruteforcing", metavar = "URL").required()
-    val dictionaryFile: String by option(help = "Path to dictionary file", metavar = "FILEPATH").default("dictionaries/alexaTop1mAXFRcommonSubdomains.txt")
+    val dictionaryFile: String by option(
+        help = "Path to dictionary file",
+        metavar = "FILEPATH"
+    ).default("dictionaries/alexaTop1mAXFRcommonSubdomains.txt")
 
     val debugFlag by option("--debug", help = "Enable debug").flag(default = false)
 
-        override fun run() {
+    override fun run() {
         val dictionary = File(dictionaryFile).readLines()
         println("Loaded dictionary of ${dictionary.size} entries")
 
         val fuzzer = VirtualHostsFuzzer(targetIP, baseDomain, dictionary, debugEnabled = debugFlag)
-        fuzzer.perform()
+        val measuredExecutionTime = measureTimeMillis {
+            fuzzer.perform()
+        }
+
+        val measuredExecutionTimeSec = measuredExecutionTime / 1000.0
+
+        println("Execution time is ${measuredExecutionTimeSec}")
+        println("Speed is ${dictionary.size / measuredExecutionTimeSec} subdomains/sec")
     }
 }
